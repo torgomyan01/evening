93cae613c445bea70391f5091cd83bbd2191cbf4
Andranik Torgomyan <and.torgomyan01@gmail.com>
2024-11-01 20:11:07 +0400

    Add slider

--- js/script.js
+++ js/script.js
@@ -13,3 +13,28 @@ const svgSaleHeader = document.querySelector('.svg-sale-header');
 setTimeout(() => {
   svgSaleHeader.classList.add('start-anim')
 }, 3000)
+
+
+const swiper = new Swiper('.extreme-img', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  slidesPerView: 1,
+  spaceBetween: 30,
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+  // breakpoints: {
+  //   992: {
+  //     slidesPerView: 4,
+  //     spaceBetween: 30,
+  //   },
+  //   0: {
+  //     slidesPerView: 1, // 1 սլայդ մոբայլում
+  //     spaceBetween: 30, // Կամ ցանկացած ցանկալի տարածություն
+  //   },
+  // },
+});
